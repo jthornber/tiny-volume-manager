68966ebc32ca2236852c377dd492847c7838dcb4
Joe Thornber <ejt@redhat.com>
2011-07-21 15:03:32 +0100

    add tests to check the validation of the data block size

--- creation_tests.rb
+++ creation_tests.rb
@@ -57,7 +57,51 @@ class CreationTests < Test::Unit::TestCase
     @dm.with_dev(table) do |pool|
       pool.message(0, "create_thin 0")
       1.upto(1000) {|i| pool.message(0, "create_snap #{i} #{i - 1}") }
-    end    
+    end
+  end
+
+  def test_non_power_of_2_data_block_size_fails
+    size = 20971520
+    table = Table.new(ThinPool.new(size, @metadata_dev, @data_dev,
+                                   @data_block_size + 57, @low_water))
+
+    assert_raises(RuntimeError) do
+      @dm.with_dev(table) do |pool|
+        # shouldn't get here
+      end
+    end
+  end
+
+  def test_too_small_data_block_size_fails
+    size = 20971520
+    table = Table.new(ThinPool.new(size, @metadata_dev, @data_dev,
+                                   64, @low_water))
+
+    assert_raises(RuntimeError) do
+      @dm.with_dev(table) do |pool|
+        # shouldn't get here
+      end
+    end
+  end
+
+  def test_too_large_data_block_size_fails
+    size = 20971520
+    table = Table.new(ThinPool.new(size, @metadata_dev, @data_dev,
+                                   2**22, @low_water))
+
+    assert_raises(RuntimeError) do
+      @dm.with_dev(table) do |pool|
+        # shouldn't get here
+      end
+    end
+  end
+
+  def test_largest_data_block_size_succeeds
+    size = 20971520
+    table = Table.new(ThinPool.new(size, @metadata_dev, @data_dev,
+                                   2**21, @low_water))
+    @dm.with_dev(table) do |pool|
+    end
   end
 end
 
