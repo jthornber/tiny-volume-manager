150b6ce769d96070dd716f2302a37e21dc760b9d
Heinz Mauelshagen <heinzm@redhat.com>
2011-07-27 14:33:29 +0200

    2nd arg to wipe_device

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,20 +1,28 @@
 require 'lib/process'
 
 module Utils
-  def wipe_device(dev_or_path)
-    size = ProcessControl.system("102400", "blockdev --getsize #{dev_or_path}").chomp.to_i
+  def wipe_device(dev_or_path, sectors = nil)
+    dev_size = ProcessControl.system("102400", "blockdev --getsize #{dev_or_path}").chomp.to_i
+
+    if sectors.nil? || dev_size < sectors
+      sectors = dev_size
+    end
 
     # We now want to calculate a reasonable block size and block count
     block_size = 2048 * 64       # 64 M
-    count = size / block_size
-    if count > 0
-      ProcessControl.run("dd if=/dev/zero of=#{dev_or_path} oflag=direct bs=64M count=#{count}")
+    if dev_size < block_size
+        block_size = dev_size
     end
 
-    remainder = size % block_size
-    if remainder != 0
+    count = sectors / block_size
+    if count > 0
+      ProcessControl.run("dd if=/dev/zero of=#{dev_or_path} oflag=direct bs=#{block_size} count=#{count}")
+    end
+
+    remainder = sectors % block_size
+    if remainder > 0
       # we have a little bit to do at the end
-      offset = block_size * (size / block_size)
+      offset = count * block_size
       ProcessControl.run("dd if=/dev/zero of=#{dev_or_path} oflag=direct bs=#{remainder * 512} count=1 seek=#{offset * 512}")
     end
   end
