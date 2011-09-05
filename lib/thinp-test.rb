3433b2aea964695f188311579f834db5c4fe439b
Joe Thornber <ejt@redhat.com>
2011-09-05 10:40:08 +0100

    undef default_test in ThinpTestCase

--- lib/thinp-test.rb
+++ lib/thinp-test.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 #----------------------------------------------------------------
 
 class ThinpTestCase < Test::Unit::TestCase
-
+  undef_method :default_test
 end
 
 #----------------------------------------------------------------
