3b37fca879baf131cd250ea697dcc79888a35e11
Joe Thornber <ejt@redhat.com>
2011-10-11 16:21:57 +0100

    fluff

--- pool_resize_tests.rb
+++ pool_resize_tests.rb
@@ -68,7 +68,7 @@ class PoolResizeTests < ThinpTestCase
           # wait until available space has been used
           event_tracker.wait do
             status = PoolStatus.new(pool)
-            status.free_data_sectors <= (@low_water_mark * @data_block_size)
+            status.free_data_sectors <= @low_water_mark * @data_block_size
           end
 
           table = Table.new(ThinPool.new(i * target_step, @metadata_dev, @data_dev,
