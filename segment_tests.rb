40759e0857dd8394e6bf6b6f71c335a3142197a7
Joe Thornber <ejt@redhat.com>
2012-03-14 11:32:07 +0000

    ExitError nonsense

--- segment_tests.rb
+++ segment_tests.rb
@@ -157,7 +157,7 @@ class SegmentTests < Test::Unit::TestCase
 
   def test_best_fit_policy5
     segs = SegmentList.new
-    assert_raise(ExitError) do
+    assert_raise(RuntimeError) do
       s = segs.alloc(AllocPolicy::BestFit.new(100))
     end
   end
