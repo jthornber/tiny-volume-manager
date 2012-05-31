b6c7aba9d38cddd37d6a844d07fc4df6a202a578
Joe Thornber <ejt@redhat.com>
2012-05-31 13:51:13 +0100

    metadata analysis

---
+++ analyse_metadata.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'config'
+require 'lib/dm'
+require 'lib/log'
+require 'lib/utils'
+require 'lib/fs'
+require 'lib/git'
+require 'lib/status'
+require 'lib/tags'
+require 'lib/thinp-test'
+require 'lib/xml_format'
+require 'lib/analysis'
+
+#----------------------------------------------------------------
+
+include XMLFormat
+
+ARGV.each do |path|
+  STDERR.puts "analysing #{path}"
+  File.open(path, 'r') do |file|
+    md = read_xml(file)
+    analysis = MetadataAnalysis.new(md)
+    analysis.block_length_histograms
+    analysis.fragmentations
+  end
+end
+
+exit(0)
+
+#----------------------------------------------------------------
