e137cde9250def479d6efdd1095e03f1499493a8
Joe Thornber <ejt@redhat.com>
2012-07-05 16:14:00 +0100

    add a PoolStatus.fail field

--- lib/status.rb
+++ lib/status.rb
@@ -4,7 +4,7 @@ require 'lib/log'
 
 class PoolStatus
   attr_reader :transaction_id, :used_metadata_blocks, :total_metadata_blocks, :used_data_blocks
-  attr_reader :total_data_blocks, :held_root, :options
+  attr_reader :total_data_blocks, :held_root, :options, :fail
 
   def parse_held_root(txt)
     case txt
@@ -49,18 +49,25 @@ class PoolStatus
   end
 
   def initialize(pool)
-    m = pool.status.match(/(\d+)\s(\d+)\/(\d+)\s(\d+)\/(\d+)\s(\S+)(\s.*)/)
+    status = pool.status
+    m = status.match(/(\d+)\s(\d+)\/(\d+)\s(\d+)\/(\d+)\s(\S+)(\s.*)/)
     if m.nil?
-      raise "couldn't get pool status"
+      # it's possible the pool's fallen back to failure mode
+      if status.match(/\s*fail\s*/)
+        @fail = true
+      else
+        raise "couldn't get pool status"
+      end
+    else
+      @fail = false
+      @transaction_id = m[1].to_i
+      @used_metadata_blocks = m[2].to_i
+      @total_metadata_blocks = m[3].to_i
+      @used_data_blocks = m[4].to_i
+      @total_data_blocks = m[5].to_i
+      @held_root = parse_held_root(m[6])
+      @options = parse_opts(m[7])
     end
-
-    @transaction_id = m[1].to_i
-    @used_metadata_blocks = m[2].to_i
-    @total_metadata_blocks = m[3].to_i
-    @used_data_blocks = m[4].to_i
-    @total_data_blocks = m[5].to_i
-    @held_root = parse_held_root(m[6])
-    @options = parse_opts(m[7])
   end
 end
 
