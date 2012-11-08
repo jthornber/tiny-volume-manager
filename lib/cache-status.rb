c64ed94c7ae48eba64a11fff0ac9eea2fbf43672
Mike Snitzer <snitzer@redhat.com>
2012-11-08 12:25:53 -0500

    revert my tweaks so I can pull in ejt's

--- lib/cache-status.rb
+++ lib/cache-status.rb
@@ -3,26 +3,26 @@ require 'lib/log'
 #----------------------------------------------------------------
 
 class CacheStatus
-  attr_reader :nr_used_blocks_metadata, :nr_blocks_metadata
-  attr_reader :read_hits, :read_misses, :write_hits, :write_misses
+  attr_reader :read_hits, :read_misses, :write_hits, :write_misses,
   attr_reader :demotions, :promotions, :residency, :nr_dirty
 
   def initialize(cache_dev)
     status = cache.status
-    m = status.match(/(\d+)\/(\d+)\s(\d+)\s(\d+)\/(\d+)\s(\d+)\/(\d+)\s(\d+)\s(\d+)\s(\d+)/)
+    m = status.match(/(\d+)\s(\d+)\/(\d+)\s(\d+)\/(\d+)\s(\d+)\s(\d+)\s(\d+)/)
     if m.nil?
       raise "couldn't parse cache status"
     else
-      @nr_used_blocks_metadata = m[1].to_i
-      @nr_blocks_metadata = m[2].to_i
-      @read_hits = m[3].to_i
-      @read_misses = m[4].to_i
-      @write_hits = m[5].to_i
-      @write_misses = m[6].to_i
-      @demotions = m[7].to_i
-      @promotions = m[8].to_i
-      @residency = m[9].to_i
-      @nr_dirty = m[10].to_i
+      attr_reader :read_hits, :read_misses, :write_hits, :write_misses,
+      attr_reader :demotions, :promotions, :residency, :nr_dirty
+
+      @read_hits = m[1].to_i
+      @read_misses = m[2].to_i
+      @write_hits = m[3].to_i
+      @write_misses = m[4].to_i
+      @demotions = m[5].to_i
+      @promotions = m[6].to_i
+      @residency = m[7].to_i
+      @nr_dirty = m[8].to_i
     end
   end
 end
