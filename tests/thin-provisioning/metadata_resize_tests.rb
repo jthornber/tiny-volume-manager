7f4d211652a284d7502f7668496725d8e4e8fb5b
Joe Thornber <ejt@redhat.com>
2013-02-11 18:38:00 +0000

    Metadata resizing tests

--- tests/thin-provisioning/metadata_resize_tests.rb
+++ tests/thin-provisioning/metadata_resize_tests.rb
@@ -6,12 +6,14 @@ require 'lib/utils'
 require 'lib/status'
 require 'lib/tags'
 require 'lib/thinp-test'
+require 'lib/disk-units'
 
 require 'pp'
 
 #----------------------------------------------------------------
 
 class MetadataResizeTests < ThinpTestCase
+  include DiskUnits
   include Tags
   include Utils
   include TinyVolumeManager
@@ -28,10 +30,8 @@ class MetadataResizeTests < ThinpTestCase
   tag :thinp_target
 
   def test_resize_metadata_no_io
-    meg = 1024 * 1024 / 512
-
-    data_size = 100 * meg
-    @tvm.add_volume(linear_vol('metadata', 1 * meg))
+    data_size = meg(100)
+    @tvm.add_volume(linear_vol('metadata', meg(1)))
     @tvm.add_volume(linear_vol('data', data_size))
 
     with_devs(@tvm.table('metadata'),
@@ -42,7 +42,7 @@ class MetadataResizeTests < ThinpTestCase
       with_dev(table) do |pool|
         status1 = PoolStatus.new(pool)
 
-        @tvm.resize('metadata', 2 * meg)
+        @tvm.resize('metadata', meg(2))
         pool.pause do
           md.pause do
             table = @tvm.table('metadata')
@@ -57,12 +57,45 @@ class MetadataResizeTests < ThinpTestCase
     end
   end
 
-  def test_exhausting_metadata_space_causes_fail_mode
-    md_blocks = 8
-    meg = 1024 * 1024  / 512
+  def test_resize_metadata_with_io
+    data_size = gig(1)
+    @tvm.add_volume(linear_vol('metadata', meg(1)))
+    @tvm.add_volume(linear_vol('data', data_size))
 
+    with_devs(@tvm.table('metadata'),
+              @tvm.table('data')) do |md, data|
+      wipe_device(md, 8)
+
+      table = Table.new(ThinPoolTarget.new(data_size, md, data, @data_block_size, @low_water_mark))
+      with_dev(table) do |pool|
+        status1 = PoolStatus.new(pool)
+
+        with_new_thin(pool, meg(50), 0) do |thin|
+          fork {wipe_device(thin)}
+          ProcessControl.sleep 5
+
+          @tvm.resize('metadata', meg(2))
+          pool.pause do
+            md.pause do
+              table = @tvm.table('metadata')
+              md.load(table)
+            end
+          end
+
+          Process.wait
+        end
+
+        status2 = PoolStatus.new(pool)
+
+        assert_equal(status1.total_metadata_blocks * 2, status2.total_metadata_blocks)
+      end
+    end
+  end
+
+  def _test_exhausting_metadata_space_causes_fail_mode
+    md_blocks = 8
     md_size = 10 * md_blocks
-    data_size = 2 * 1024 * meg
+    data_size = gig(2)
 
     @tvm.add_volume(linear_vol('metadata', md_size))
     @tvm.add_volume(linear_vol('data', data_size))
@@ -82,12 +115,15 @@ class MetadataResizeTests < ThinpTestCase
     end
   end
 
-  def test_low_metadata_space_triggers_event
-    md_blocks = 8
-    meg = 1024 * 1024  / 512
+  # It's hard to predict how much metadata will be used by a
+  # particular operation.  So the approach we'll take is to set up a
+  # pool, do some work, query to see how much is used and then set the
+  # thresholds appropriately.
 
-    md_size = 10 * md_blocks
-    data_size = 2 * 1024 * meg
+  def _test_low_metadata_space_triggers_event
+    md_blocks = 8
+    md_size = 128 * md_blocks
+    data_size = gig(2)
 
     @tvm.add_volume(linear_vol('metadata', md_size))
     @tvm.add_volume(linear_vol('data', data_size))
@@ -108,8 +144,6 @@ class MetadataResizeTests < ThinpTestCase
       end
     end
   end
-
-  # FIXME: test low md triggers if passed as part of initial format
  end
 
 #----------------------------------------------------------------
