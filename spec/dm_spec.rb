add624f1dae7264ba20dfd69570bdd2ca3f6144b
Joe Thornber <ejt@redhat.com>
2013-02-21 13:54:17 +0000

    Tidy dm_spec.rb

--- spec/dm_spec.rb
+++ spec/dm_spec.rb
@@ -3,143 +3,122 @@ require 'device-mapper/dm'
 
 #----------------------------------------------------------------
 
-include DM
-
-def new_dev(name)
-  dm = mock('dm interface')
-  dev = DMDev.new(name, dm)
-  [dev, dm]
-end
-
-def hands_down(method, *args)
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(method).with(dev.path, *args)
-    dev.send(method, *args)
-end
-
-def hands_down_with_result(method, result, *args)
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(method).with(dev.path, *args).and_return(result)
-    dev.send(method, *args).should == result
-end
-
 describe DM::DMDev do
+  include DM
+
+  before :each do
+    @name = 'foo'
+    @dm = mock('dm interface')
+    @dev = DMDev.new(@name, @dm)
+  end
+
+  def hands_down(method, *args)
+    @dm.should_receive(method).with(@dev.path, *args)
+    @dev.send(method, *args)
+  end
+
+  def hands_down_with_result(method, result, *args)
+    @dm.should_receive(method).with(@dev.path, *args).and_return(result)
+    @dev.send(method, *args).should == result
+  end
+
   it "should have a read-only name" do
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dev.name.should == name
-    expect {dev.name = 'bar'}.to raise_error(NoMethodError)
+    @dev.name.should == @name
+    expect {@dev.name = 'bar'}.to raise_error(NoMethodError)
   end
 
   it "should know where it's node is in the /dev tree" do
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dev.path.should == "/dev/mapper/#{name}"
+    @dev.path.should == "/dev/mapper/#{@name}"
   end
 
-  it "should have a suspend method" do
+  it "should hand down suspend" do
     hands_down(:suspend)
   end
 
-  it "should have a resume method" do
+  it "should hand down resume" do
     hands_down(:resume)
   end
 
   it "should have a pause method that combines suspend/resume" do
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:suspend).with(dev.path)
-    dev.pause do
-      dm.should_receive(:resume).with(dev.path)
+    @dm.should_receive(:suspend).with(@dev.path)
+    @dev.pause do
+      @dm.should_receive(:resume).with(@dev.path)
     end
   end
 
-  it "should have a remove method" do
+  it "should hand down remove" do
     hands_down(:remove)
   end
 
-  it "should have a message method" do
+  it "should hand down message" do
     args = [123, 'one', 'two', 'three']
     hands_down_with_result(:message, true, *args)
   end
 
-  it "should have a status method" do
+  it "should hand down status" do
     hands_down_with_result(:status, "ksldfk sldj sldjs ")
   end
 
-  it "should have a table method" do
+  it "should hand down table" do
     hands_down_with_result(:table, "lsdkfjs sdljs sld fs\nsld f sdlkfjs ")
   end
 
-  it "should have an info method" do
+  it "should hand down info" do
     hands_down_with_result(:info, "iwekoweiurwo ")
   end
 
-  it "should have a dm_name method" do
+  it "should know it's dm-# name" do
     fake_info = "the quick brown fox\nMajor, minor: 45, 17\njumps over the lazy"
 
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:info).
-      with("/dev/mapper/#{name}").
+    @dm.should_receive(:info).
+      with("/dev/mapper/#{@name}").
       and_return(fake_info)
-    dev.dm_name.should == 'dm-17'
+    @dev.dm_name.should == 'dm-17'
   end
 
   it "should raise if malformed info in dm_name" do
     fake_info = "the quick brown fox\nMajor, minor: , 17\njumps over the lazy"
 
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:info).
-      with("/dev/mapper/#{name}").
+    @dm.should_receive(:info).
+      with("/dev/mapper/#{@name}").
       and_return(fake_info)
-    expect {dev.dm_name}.to raise_error(RuntimeError)
+    expect {@dev.dm_name}.to raise_error(RuntimeError)
   end
 
   it "should have an event_nr method" do
     fake_status = "the quick brown fox\nEvent number: 18 \njumps over the lazy"
 
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:status).
-      with("/dev/mapper/#{name}", '-v').
+    @dm.should_receive(:status).
+      with("/dev/mapper/#{@name}", '-v').
       and_return(fake_status)
-    dev.event_nr.should == 18
+    @dev.event_nr.should == 18
   end
 
   it "should raise if malformed status in event_nr" do
     fake_status = "the quick brown fox\nEvent nimber: 18 \njumps over the lazy"
 
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:status).
-      with("/dev/mapper/#{name}", '-v').
+    @dm.should_receive(:status).
+      with("/dev/mapper/#{@name}", '-v').
       and_return(fake_status)
-    expect {dev.event_nr}.to raise_error(RuntimeError)
+    expect {@dev.event_nr}.to raise_error(RuntimeError)
   end
 
   it "should have a to_s method" do
-    dev, dm = new_dev('foo')
-    dev.to_s.should == dev.path
+    @dev.to_s.should == @dev.path
   end
 
   it "should have event_tracker method" do
     fake_status = "the quick brown fox\nEvent number: 18 \njumps over the lazy"
 
-    name = 'foo'
-    dev, dm = new_dev(name)
-    dm.should_receive(:status).
-      with("/dev/mapper/#{name}", '-v').
+    @dm.should_receive(:status).
+      with("/dev/mapper/#{@name}", '-v').
       and_return(fake_status)
-    tracker = dev.event_tracker
+    tracker = @dev.event_tracker
     tracker.event_nr.should == 18
-    tracker.device.should === dev
+    tracker.device.should === @dev
   end
 
-  it "should have a wait method" do
+  it "should hand down wait" do
     hands_down(:wait, 18)
   end
 
